343eb958c0edb453fbc8dc4e4c3f3bed3c7193c6
Kenneth3750 <110206946+Kenneth3750@users.noreply.github.com>
2023-09-04 21:58:38 -0500

    Update web_page.py
    date time decoded

--- diseño/web_page.py
+++ diseño/web_page.py
@@ -2,7 +2,13 @@ from flask import Flask, render_template, request, jsonify
 import mysql.connector
 from mysql.connector import Error
 import datetime
+import pytz  # Importa la biblioteca pytz
+
 app = Flask(__name__)
+
+# Define la zona horaria de Bogotá
+bogota_timezone = pytz.timezone('America/Bogota')
+
 def database_connect():
     try:
         connection = mysql.connector.connect(
@@ -16,7 +22,7 @@ def database_connect():
     except Error as e:
         print("Database unreachable, " +e)
         return None
-    
+
 @app.route('/components', methods=['GET'])
 def data():
     try: 
@@ -28,14 +34,24 @@ def data():
             result = cursor.fetchall()
             cursor.close()
             connect.close()
+
+            # Decodifica el timestamp Unix Epoch a la zona horaria de Bogotá
+            for row in result:
+                timestamp = row['Time_stamp']
+                if timestamp is not None:
+                    timestamp = datetime.datetime.fromtimestamp(timestamp, pytz.utc)
+                    timestamp = timestamp.astimezone(bogota_timezone)
+                    row['Time_stamp'] = timestamp.strftime('%Y-%m-%d %H:%M:%S %Z')
+
             return jsonify(result)
         else:
             return "Database unreachable"
     except Exception as e:
         return "Error " + str(e)
-    
+
 @app.route('/')
 def index():
     return render_template("index.html")
+
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=80)
