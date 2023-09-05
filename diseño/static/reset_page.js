264b0318174ef2d5fd7039af5131c9da4e0132ad
Kenneth3750 <110206946+Kenneth3750@users.noreply.github.com>
2023-09-04 20:58:34 -0500

    Update reset_page.js
    Intento 2 del marker

--- diseño/static/reset_page.js
+++ diseño/static/reset_page.js
@@ -2,18 +2,21 @@ var map; // Declara una variable global para el mapa
 var marker; // Declara una variable global para el marcador
 
 function initializeMap(latitude, longitude) {
-  
+    // Crea el mapa y centra en las coordenadas
     map = L.map('map').setView([latitude, longitude], 16);
     
-  
+    // Agrega una capa de azulejos de OpenStreetMap
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         maxZoom: 19,
     }).addTo(map);
 
-  
-    marker = L.marker([latitude, longitude], { draggable: false }).addTo(map)
+    // Agrega un marcador en las coordenadas
+    marker = L.marker([latitude, longitude], { draggable: true, rotationAngle: 0 }).addTo(map)
         .bindPopup('Last Location!')
         .openPopup();
+
+    // Establece la rotación del marcador en 0 grados (norte)
+    marker.setRotationAngle(0);
 }
 
 function reloadTable() {
@@ -29,17 +32,12 @@ function reloadTable() {
                 } else {
                     // Actualiza la posición del marcador
                     marker.setLatLng([lastLocation.Latitude, lastLocation.Longitude]);
+
+                 
+                    var rotationAngle = 0; // Ajusta el ángulo según tus necesidades
+                    marker.setRotationAngle(rotationAngle);
                 }
             }
-            // Actualiza la tabla como lo hacías antes
-            var tablaHTML = "<table>";
-            tablaHTML += "<thead><tr><th>ID</th><th>Latitude</th><th>Longitude</th><th>Time_stamp</th></tr></thead>";
-            tablaHTML += "<tbody>";
-            response.forEach(function(row) {
-                tablaHTML += "<tr><td>" + row.ID + "</td><td>" + row.Latitude + "</td><td>" + row.Longitude + "</td><td>" + row.Time_stamp + "</td></tr>";
-            });
-            tablaHTML += "</tbody></table>";
-            $("#tabla-contenido").html(tablaHTML);
         },
         error: function(xhr, status, error) {
             console.error("AJAX request failed", error);
