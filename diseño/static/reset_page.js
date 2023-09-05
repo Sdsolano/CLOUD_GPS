7533884814f2e6ed6b6e45d6c5fb5139ea779e84
Kenneth3750 <110206946+Kenneth3750@users.noreply.github.com>
2023-09-04 21:27:52 -0500

    Update reset_page.js

--- diseño/static/reset_page.js
+++ diseño/static/reset_page.js
@@ -1,14 +1,17 @@
-var map; // Declara una variable global para el mapa
-var marker; // Declara una variable global para el marcador
+<script>
+var map;
+var marker;
 
 function initializeMap(latitude, longitude) {
-    // Crea el mapa y centra en las coordenadas
     map = L.map('map').setView([latitude, longitude], 16);
-    
-    // Agrega una capa de azulejos de OpenStreetMap
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         maxZoom: 19,
     }).addTo(map);
+    
+    marker = L.marker([latitude, longitude], { rotationAngle: 0 }).addTo(map)
+        .bindPopup('Last Location!')
+        .openPopup();
+    marker.setRotationAngle(0);
 }
 
 function reloadTable() {
@@ -16,38 +19,25 @@ function reloadTable() {
         url: "/components",
         method: "GET",
         success: function(response) {
+            var tablaHTML = "<table>";
+            tablaHTML += "<thead><tr><th>ID</th><th>Latitude</th><th>Longitude</th><th>Time_stamp</th></tr></thead>";
+            tablaHTML += "<tbody>";
+            response.forEach(function(row) {
+                tablaHTML += "<tr><td>" + row.ID + "</td><td>" + row.Latitude + "</td><td>" + row.Longitude + "</td><td>" + row.Time_stamp + "</td></tr>";
+            });
+            tablaHTML += "</tbody></table>";
+            $("#tabla-contenido").html(tablaHTML);
+
             if (response.length > 0) {
                 var lastLocation = response[0];
-                // Si el mapa no se ha inicializado, inicialízalo
                 if (!map) {
                     initializeMap(lastLocation.Latitude, lastLocation.Longitude);
-                }
-
-                // Actualiza la posición del marcador
-                if (!marker) {
-                    marker = L.marker([lastLocation.Latitude, lastLocation.Longitude]).addTo(map)
-                        .bindPopup('Last Location!')
-                        .openPopup();
                 } else {
                     marker.setLatLng([lastLocation.Latitude, lastLocation.Longitude]);
+                    var rotationAngle = 0; // Ajusta el ángulo según tus necesidades
+                    marker.setRotationAngle(rotationAngle);
+                    map.setView([lastLocation.Latitude, lastLocation.Longitude]); // Centra el mapa en las coordenadas
                 }
-
-                // Calcula el ángulo de rotación del marcador (si es necesario)
-                // Esto depende de la dirección en la que deseas que apunte el marcador
-                // Por ejemplo, si quieres que apunte hacia el norte, usa 0 grados
-                // Si quieres que apunte hacia el este, usa 90 grados, etc.
-                var rotationAngle = 0; // Ajusta el ángulo según tus necesidades
-                marker.setRotationAngle(rotationAngle);
-                
-                // Actualiza la tabla como lo hacías antes
-                var tablaHTML = "<table>";
-                tablaHTML += "<thead><tr><th>ID</th><th>Latitude</th><th>Longitude</th><th>Time_stamp</th></tr></thead>";
-                tablaHTML += "<tbody>";
-                response.forEach(function(row) {
-                    tablaHTML += "<tr><td>" + row.ID + "</td><td>" + row.Latitude + "</td><td>" + row.Longitude + "</td><td>" + row.Time_stamp + "</td></tr>";
-                });
-                tablaHTML += "</tbody></table>";
-                $("#tabla-contenido").html(tablaHTML);
             }
         },
         error: function(xhr, status, error) {
@@ -58,6 +48,6 @@ function reloadTable() {
 
 $(document).ready(function() {
     reloadTable();
-
     setInterval(reloadTable, 7000);
 });
+</script>
