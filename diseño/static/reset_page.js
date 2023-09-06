5abc7046b98c61f8db8a627a0250b3f45948e4d8
Kenneth3750 <110206946+Kenneth3750@users.noreply.github.com>
2023-09-05 23:14:42 -0500

    Update reset_page.js

--- diseño/static/reset_page.js
+++ diseño/static/reset_page.js
@@ -1,70 +1,74 @@
 let map;
 let marker;
 
-function initMap() {
-    // Inicializa el mapa dentro de esta función
-    map = new google.maps.Map(document.getElementById('map'), {
-        center: { lat: -0.5, lng: 0.5 }, // Coordenadas iniciales de ejemplo
-        zoom: 13,
-        minZoom: 12,
-    });
-
-    // Crea el marcador en el mapa
-    marker = new google.maps.Marker({
-        position: { lat: -0.5, lng: 0.5 }, // Coordenadas iniciales de ejemplo
-        map: map,
-        title: "Mi Marcador"
-    });
-}
-
-function updateMapAndTable(response) {
-    if (!map) {
-        // Si el mapa aún no se ha inicializado, llama a initMap
-        initMap();
-    }
-
-    // Actualiza el mapa y el marcador aquí
-    if (response.length > 0) {
-        var lastRow = response[response.length - 1];
-        var lastLatitude = parseFloat(lastRow.Latitude);
-        var lastLongitude = parseFloat(lastRow.Longitude);
-
-        if (!isNaN(lastLatitude) && !isNaN(lastLongitude)) {
-            // Actualiza la posición del marcador
-            marker.setPosition(new google.maps.LatLng(lastLatitude, lastLongitude));
-
-            // Centra el mapa en la nueva ubicación
-            map.setCenter(new google.maps.LatLng(lastLatitude, lastLongitude));
-
-            console.log("Last Latitude:", lastLatitude);
-            console.log("Last Longitude:", lastLongitude);
-        } else {
-            console.error("Las coordenadas no son números válidos.");
-        }
-    }
-}
-
 function reloadTable() {
+    // Define la función initMap dentro de reloadTable
+    function initMap() {
+        // Inicializa el mapa dentro de esta función
+        map = new google.maps.Map(document.getElementById('map'), {
+            center: { lat: -0.5, lng: 0.5 }, // Coordenadas iniciales de ejemplo
+            zoom: 13,
+            minZoom: 12,
+        });
+
+        // Crea el marcador en el mapa
+        marker = new google.maps.Marker({
+            position: { lat: -0.5, lng: 0.5 }, // Coordenadas iniciales de ejemplo
+            map: map,
+            title: "Mi Marcador"
+        });
+    }
+
     $.ajax({
         url: "/components",
         method: "GET",
         success: function (response) {
             if (typeof google !== 'undefined' && typeof google.maps !== 'undefined') {
-                // Verifica si la API de Google Maps se ha cargado correctamente
+                // Verifica si la API de Google Maps se ha cargado
 
-                // Actualiza el mapa y la tabla
-                updateMapAndTable(response);
+                if (!map) {
+                    // Si el mapa aún no se ha inicializado, llama a initMap
+                    initMap();
+                }
 
-                // Actualiza la tabla
-                var tablaHTML = "<table>";
-                tablaHTML += "<thead><tr><th>ID</th><th>Latitude</th><th>Longitude</th></tr></thead>";
-                tablaHTML += "<tbody>";
-                response.forEach(function(row) {
-                    tablaHTML += "<tr><td>" + row.ID + "</td><td>" + row.Latitude + "</td><td>" + row.Longitude + "</td></tr>";
-                });
-                tablaHTML += "</tbody></table>";
+                // Actualiza el mapa y el marcador aquí
+                if (response.length > 0) {
+                    var lastRow = response[response.length - 1];
+                    var lastLatitude = parseFloat(lastRow.Latitude);
+                    var lastLongitude = parseFloat(lastRow.Longitude);
+                    var lastTime = parseFloat(lastRow.Time_stamp);
+                    var tablaHTML = "<table>";
+                    tablaHTML += "<thead><tr><th>ID</th><th>Latitude</th><th>Longitude</th><th>Time_stamp</th></tr></thead>";
+                    tablaHTML += "<tbody>";
+                    response.forEach(function(row) {
+                        tablaHTML += "<tr><td>" + row.ID + "</td><td>" + row.Latitude + "</td><td>" + row.Longitude +  "</td><td>" + row.Time_stamp + "</td></tr>";
+                    });
+                    tablaHTML += "</tbody></table>";
 
-                $("#tabla-contenido").html(tablaHTML);
+                    $("#tabla-contenido").html(tablaHTML);
+
+                    if (!isNaN(lastLatitude) && !isNaN(lastLongitude)) {
+                        // Actualiza la posición del marcador
+                        marker.setPosition(new google.maps.LatLng(lastLatitude, lastLongitude));
+                        let contenidoMarcador = "Latitude:" + lastLatitude + "<br> Longitude:" + lastLongitude + "<br> Timestamp" + lastTime;
+                        let infoWindow = new google.maps.InfoWindow({
+                            content: contenidoMarcador
+                        });
+
+                        // Abre el InfoWindow cuando se hace clic en el marcador
+                        marker.addListener('click', function() {
+                            infoWindow.open(map, marker);
+                        });
+
+                        // Centra el mapa en la nueva ubicación
+                        map.setCenter(new google.maps.LatLng(lastLatitude, lastLongitude));
+
+                        console.log("Last Latitude:", lastLatitude);
+                        console.log("Last Longitude:", lastLongitude);
+                    } else {
+                        console.error("Las coordenadas no son números válidos.");
+                    }
+                }
             } else {
                 console.error("La API de Google Maps no se ha cargado correctamente.");
             }
@@ -76,9 +80,6 @@ function reloadTable() {
 }
 
 $(document).ready(function () {
-    // Llama a initMap para inicializar el mapa al cargar la página
-    initMap();
-
     // Llama a reloadTable al cargar la página
     reloadTable();
 
