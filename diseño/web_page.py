67adf4a22ff039b763b7ad0ac1da7371514a57ad
Kenneth3750 <110206946+Kenneth3750@users.noreply.github.com>
2023-09-08 23:35:29 -0500

    Update web_page.py

--- diseño/web_page.py
+++ diseño/web_page.py
@@ -12,10 +12,10 @@ bogota_timezone = pytz.timezone('America/Bogota')
 def database_connect():
     try:
         connection = mysql.connector.connect(
-           'host': os.environ.get('DB_HOST'),
-           'user': os.environ.get('DB_USER'),
-           'password': os.environ.get('DB_PASSWORD'),
-           'database': os.environ.get('DB_NAME')
+            host=os.environ.get('DB_HOST'),
+            user=os.environ.get('DB_USER'),
+            password=os.environ.get('DB_PASSWORD'),
+            database=os.environ.get('DB_NAME')
         )
        
         return connection
