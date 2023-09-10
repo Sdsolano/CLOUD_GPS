bc9d51a0bb89205397603543c75a2028a5f7414f
Adrián Pabón Mendoza <adrianpabonmendoza@gmail.com>
2023-09-10 17:15:24 -0500

    Update reset_page.js

--- diseño/static/reset_page.js
+++ diseño/static/reset_page.js
@@ -1,13 +1,12 @@
 let map;
 let marker;
-let polyline;
-let smoothedPath = new google.maps.MVCArray(); // Usaremos MVCArray para una polilínea suave
+let polyline = null;
 
 function initMap() {
     // Inicializa el mapa
     map = new google.maps.Map(document.getElementById('map'), {
         center: { lat: -0.5, lng: 0.5 }, // Coordenadas iniciales de ejemplo
-        zoom: 15,
+        zoom: 13,
         minZoom: 12,
     });
 
@@ -16,16 +15,6 @@ function initMap() {
         position: { lat: -0.5, lng: 0.5 }, // Coordenadas iniciales de ejemplo
         map: map,
         title: "Mi Marcador"
-    });
-
-    // Crea una polilínea suave con MVCArray
-    polyline = new google.maps.Polyline({
-        path: smoothedPath,
-        geodesic: true,
-        strokeColor: '#FF0000',
-        strokeOpacity: 1.0,
-        strokeWeight: 2,
-        map: map,
     });
 
     // Carga la tabla y actualiza el mapa
@@ -42,9 +31,46 @@ function reloadTable() {
 
                 // Actualiza la tabla con los últimos tres datos
                 var tablaHTML = "<table>";
-                tablaHTML += "<thead><tr><th>ID</th><th>Latitude</th><th>Longitude
+                tablaHTML += "<thead><tr><th>ID</th><th>Latitude</th><th>Longitude</th><th>Time_stamp</th></tr></thead>";
+                tablaHTML += "<tbody>";
+                for (var i = 0; i < Math.min(response.length, 3); i++) {
+                    var row = response[i];
+                    tablaHTML += "<tr><td>" + row.ID + "</td><td>" + row.Latitude + "</td><td>" + row.Longitude +  "</td><td>" + row.Time_stamp + "</td></tr>";
+                }
+                tablaHTML += "</tbody></table>";
 
+                $("#tabla-contenido").html(tablaHTML);
+
+                if (response.length > 0) {
+                    var firstRow = response[0];
+                    var firstLatitude = parseFloat(firstRow.Latitude);
+                    var firstLongitude = parseFloat(firstRow.Longitude);
+
+                    if (!isNaN(firstLatitude) && !isNaN(firstLongitude)) {
+                        // Actualiza la posición del marcador con las coordenadas de la primera fila
+                        marker.setPosition(new google.maps.LatLng(firstLatitude, firstLongitude));
+
+                        // Centra el mapa en la ubicación de la primera fila
+                        map.setCenter(new google.maps.LatLng(firstLatitude, firstLongitude));
+
+                        // polilíneas 
+                        
+                    } else {
+                        console.error("Las coordenadas de la primera fila no son números válidos.");
+                    }
+                }
+            } else {
+                console.error("La API de Google Maps no se ha cargado correctamente.");
+            }
+        },
+        error: function (xhr, status, error) {
+            console.error("AJAX request failed", error);
+        }
+    });
+}
 
 $(document).ready(function () {
+    
     setInterval(reloadTable, 7000);
 });
+
