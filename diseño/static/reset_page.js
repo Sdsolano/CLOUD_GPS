423fdf92050ada33ca8f6c173f94221a5a63aec5
Adrián Pabón Mendoza <adrianpabonmendoza@gmail.com>
2023-09-10 12:16:40 -0500

    case 1

--- diseño/static/reset_page.js
+++ diseño/static/reset_page.js
@@ -24,7 +24,7 @@ function initMap() {
         strokeColor: '#FF0000', 
         strokeOpacity: 1.0, 
         strokeWeight: 2,
-        map: map
+        map: map,
     })
 
     // Carga la tabla y actualiza el mapa
@@ -38,6 +38,50 @@ function reloadTable() {
         success: function (response) {
             if (typeof google !== 'undefined' && typeof google.maps !== 'undefined') {
                 // Verifica si la API de Google Maps se ha cargado
+                function reloadTable() {
+    $.ajax({
+        url: "/components",
+        method: "GET",
+        success: function (response) {
+            if (typeof google !== 'undefined' && typeof google.maps !== 'undefined') {
+                // Verifica si la API de Google Maps se ha cargado
+                polyline.setPath([])
+                // Actualiza la tabla con los últimos tres datos
+                var tablaHTML = "<table>";
+                tablaHTML += "<thead><tr><th>ID</th><th>Latitude</th><th>Longitude</th><th>Time_stamp</th></tr></thead>";
+                tablaHTML += "<tbody>";
+                for (var i = 0; i < Math.min(response.length, 3); i++) {
+                    var row = response[i];
+                    tablaHTML += "<tr><td>" + row.ID + "</td><td>" + row.Latitude + "</td><td>" + row.Longitude +  "</td><td>" + row.Time_stamp + "</td></tr>";
+                }
+                tablaHTML += "</tbody></table>";
+
+                $("#tabla-contenido").html(tablaHTML);
+
+                if (response.length > 0) {
+                    var firstRow = response[0];
+                    var firstLatitude = parseFloat(firstRow.Latitude);
+                    var firstLongitude = parseFloat(firstRow.Longitude);
+
+                    if (!isNaN(firstLatitude) && !isNaN(firstLongitude)) {
+                        // Actualiza la posición del marcador con las coordenadas de la primera fila
+                        marker.setPosition(new google.maps.LatLng(firstLatitude, firstLongitude));
+
+                        // Centra el mapa en la ubicación de la primera fila
+                        map.setCenter(new google.maps.LatLng(firstLatitude, firstLongitude));
+                    } else {
+                        console.error("Las coordenadas de la primera fila no son números válidos.");
+                    }
+                }
+            } else {
+                console.error("La API de Google Maps no se ha cargado correctamente.");
+            }
+        },
+        error: function (xhr, status, error) {
+            console.error("AJAX request failed", error);
+        }
+    });
+}
 
                 // Actualiza la tabla con los últimos tres datos
                 var tablaHTML = "<table>";
@@ -59,6 +103,8 @@ function reloadTable() {
                     if (!isNaN(firstLatitude) && !isNaN(firstLongitude)) {
                         // Actualiza la posición del marcador con las coordenadas de la primera fila
                         marker.setPosition(new google.maps.LatLng(firstLatitude, firstLongitude));
+                        polyline.getPath().push(new google.maps.LatLng(firstLatitude, firstLongitude));
+
 
                         // Centra el mapa en la ubicación de la primera fila
                         map.setCenter(new google.maps.LatLng(firstLatitude, firstLongitude));
