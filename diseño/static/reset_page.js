2a7144521a62d26a19e656b5c8d9e335b9d5b3e0
Adrián Pabón Mendoza <adrianpabonmendoza@gmail.com>
2023-09-10 16:50:17 -0500

    Update reset_page.js
    este es el funcional

--- diseño/static/reset_page.js
+++ diseño/static/reset_page.js
@@ -1,106 +1,23 @@
 let map;
 let marker;
-let polyline;
-let smoothedPath = new google.maps.MVCArray(); // Usaremos MVCArray para una polilínea suave
-var previousMarkerPosition = null;
-let markerAtTip;
-
 
 function initMap() {
     // Inicializa el mapa
     map = new google.maps.Map(document.getElementById('map'), {
         center: { lat: -0.5, lng: 0.5 }, // Coordenadas iniciales de ejemplo
-        position: { lat: -0.5, lng: 0.5 },
-        zoom: 15,
+        zoom: 13,
         minZoom: 12,
     });
 
     // Crea el marcador en el mapa
     marker = new google.maps.Marker({
-       position: { lat: -0.5, lng: 0.5 }, // Coordenadas iniciales de ejemplo
+        position: { lat: -0.5, lng: 0.5 }, // Coordenadas iniciales de ejemplo
         map: map,
         title: "Mi Marcador"
     });
 
-    // Crea una polilínea suave con MVCArray
-    polyline = new google.maps.Polyline({
-    path: smoothedPath, // Aquí debes usar "smoothedPath" en lugar de "position"
-    geodesic: true,
-    strokeColor: '#FF0000',
-    strokeOpacity: 1.0,
-    strokeWeight: 2,
-    map: map,
-});
-
-   markerAtTip = new google.maps.Marker({
-    map: map,
-    icon: {
-        path: google.maps.SymbolPath.CIRCLE,
-        scale: 20,
-        fillColor: '#FF0000',
-        fillOpacity: 1,
-        strokeWeight: 0
-    }
-});
-
-
     // Carga la tabla y actualiza el mapa
     reloadTable();
-    loadCoordinatesFromDatabase();
-    loadPolylineFromLocalStorage();
-
-    google.maps.event.addListener(map, 'center_changed', function() {
-        marker.setPosition(map.getCenter());
-    });
-    
-
-    
-}
-
-function savePolylineToLocalStorage() {
-    var polylineArray = smoothedPath.getArray().map(function (latLng) {
-        return { lat: latLng.lat(), lng: latLng.lng() };
-    });
-    localStorage.setItem('polyline', JSON.stringify(polylineArray));
-}
-
-// Carga la polilínea desde el almacenamiento local
-function loadPolylineFromLocalStorage() {
-    var polylineArray = JSON.parse(localStorage.getItem('polyline'));
-    if (Array.isArray(polylineArray)) {
-        smoothedPath.clear();
-        polylineArray.forEach(function (coord) {
-            smoothedPath.push(new google.maps.LatLng(coord.lat, coord.lng));
-        });
-    }
-}
-
-function loadCoordinatesFromDatabase() {
-    $.ajax({
-        url: "/components", // Reemplaza esto con la URL correcta de tu servidor
-        method: "GET",
-        success: function (response) {
-            if (Array.isArray(response) && response.length > 0) {
-                // Limpia la polilínea existente antes de cargar nuevas coordenadas
-                smoothedPath.clear();
-
-                // Recorre las coordenadas y agrégalas a la polilínea
-                response.forEach(function (coord) {
-                    smoothedPath.push(new google.maps.LatLng(coord.lat, coord.lng));
-                });
-
-                // Mueve el marcador al último punto de la polilínea
-                if (smoothedPath.getLength() > 0) {
-                    markerAtTip.setPosition(smoothedPath.getAt(smoothedPath.getLength() - 1));
-                }
-            }
-        },
-        error: function (xhr, status, error) {
-            console.error("Error al cargar coordenadas desde la base de datos", error);
-        }
-    });
-
-
 }
 
 function reloadTable() {
@@ -129,19 +46,11 @@ function reloadTable() {
                     var firstLongitude = parseFloat(firstRow.Longitude);
 
                     if (!isNaN(firstLatitude) && !isNaN(firstLongitude)) {
-
-                        // Centra el mapa en la ubicación de la primera fila
-                        map.setCenter(new google.maps.LatLng(firstLatitude, firstLongitude));
-
                         // Actualiza la posición del marcador con las coordenadas de la primera fila
                         marker.setPosition(new google.maps.LatLng(firstLatitude, firstLongitude));
 
-                        // Agrega la nueva posición a la polilínea suave
-                        
-                        smoothedPath.push(new google.maps.LatLng(firstLatitude, firstLongitude));
-                        markerAtTip.setPosition(smoothedPath.getAt(smoothedPath.getLength() - 1));
-
-
+                        // Centra el mapa en la ubicación de la primera fila
+                        map.setCenter(new google.maps.LatLng(firstLatitude, firstLongitude));
                     } else {
                         console.error("Las coordenadas de la primera fila no son números válidos.");
                     }
@@ -157,5 +66,6 @@ function reloadTable() {
 }
 
 $(document).ready(function () {
+    
     setInterval(reloadTable, 7000);
 });
