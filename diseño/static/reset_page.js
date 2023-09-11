db0fd29df2870342373773e030fecc788a255be5
Samuel Solano <114268063+Sdsolano@users.noreply.github.com>
2023-09-11 17:47:14 -0500

    Update reset_page.js

--- diseño/static/reset_page.js
+++ diseño/static/reset_page.js
@@ -6,7 +6,7 @@ function initMap() {
 
     // Agrega un mapa base de OpenStreetMap (puedes cambiarlo a otro proveedor de mapas)
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        maxZoom: 20, // Nivel de zoom máximo
+        maxZoom: 18, // Nivel de zoom máximo
         minZoom: 5, // Nivel de zoom mínimo
     }).addTo(map);
 
