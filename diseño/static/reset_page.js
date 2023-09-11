7d47c1197a111d61d3bcec17af5b9a8e921bce10
Adrián Pabón Mendoza <adrianpabonmendoza@gmail.com>
2023-09-11 18:52:23 -0500

    Update reset_page.js

--- diseño/static/reset_page.js
+++ diseño/static/reset_page.js
@@ -1,55 +1,57 @@
-var map;
-var marker;
+let map;
+        let marker;
 
-function initMap() {
-    // Inicializa el mapa
-    map = L.map('map').setView([10.0, -74.0], 13);
-    map.setView([10.0, -74.0]);
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        maxZoom: 19,
-        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-    }).addTo(map);
+        function initMap() {
+            // Inicializa el mapa
+            map = L.map('map').setView([10.0, -74.0], 13);
+            L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+                maxZoom: 19,
+                attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            }).addTo(map);
 
-    // Agrega un marcador inicial
-    marker = L.marker([10.0, -74.0]).addTo(map);
-}
+            // Agrega un marcador inicial
+            marker = L.marker([10.0, -74.0]).addTo(map);
 
-function reloadTable() {
-    $.ajax({
-        url: "/components",
-        method: "GET",
-        success: function (response) {
-            // Actualiza la tabla con los últimos tres datos
-            var tablaHTML = "<table>";
-            tablaHTML += "<thead><tr><th>ID</th><th>Latitude</th><th>Longitude</th><th>Time_stamp</th></tr></thead>";
-            tablaHTML += "<tbody>";
-            for (var i = 0; i < Math.min(response.length, 3); i++) {
-                var row = response[i];
-                tablaHTML += "<tr><td>" + row.ID + "</td><td>" + row.Latitude + "</td><td>" + row.Longitude +  "</td><td>" + row.Time_stamp + "</td></tr>";
-            }
-            tablaHTML += "</tbody></table>";
-
-            $("#tabla-contenido").html(tablaHTML);
-
-            if (response.length > 0) {
-                var coords = [parseFloat(response[0].Latitude), parseFloat(response[0].Longitude)];
-
-                // Actualiza la posición del marcador con las coordenadas de la primera fila
-                marker.setLatLng(coords).update();
-
-                // Centra el mapa en las nuevas coordenadas
-                map.setView(coords);
-            } else {
-                console.error("No se encontraron datos para mostrar en el mapa.");
-            }
-        },
-        error: function (xhr, status, error) {
-            console.error("AJAX request failed", error);
+            // Carga la tabla y actualiza el mapa
+            reloadTable();
         }
-    });
-}
 
-$(document).ready(function () { 
-     initMap();
-    setInterval(reloadTable, 7000);
-});
+        function reloadTable() {
+            $.ajax({
+                url: "/components",
+                method: "GET",
+                success: function (response) {
+                    // Actualiza la tabla con los últimos tres datos
+                    var tablaHTML = "<table>";
+                    tablaHTML += "<thead><tr><th>ID</th><th>Latitude</th><th>Longitude</th><th>Time_stamp</th></tr></thead>";
+                    tablaHTML += "<tbody>";
+                    for (var i = 0; i < Math.min(response.length, 3); i++) {
+                        var row = response[i];
+                        tablaHTML += "<tr><td>" + row.ID + "</td><td>" + row.Latitude + "</td><td>" + row.Longitude +  "</td><td>" + row.Time_stamp + "</td></tr>";
+                    }
+                    tablaHTML += "</tbody></table>";
+
+                    $("#tabla-contenido").html(tablaHTML);
+
+                    if (response.length > 0) {
+                        var coords = [parseFloat(response[0].Latitude), parseFloat(response[0].Longitude)];
+
+                        // Actualiza la posición del marcador con las coordenadas de la primera fila
+                        marker.setLatLng(coords);
+
+                        // Centra el mapa en las nuevas coordenadas
+                        map.setView(coords);
+                    } else {
+                        console.error("No se encontraron datos para mostrar en el mapa.");
+                    }
+                },
+                error: function (xhr, status, error) {
+                    console.error("AJAX request failed", error);
+                }
+            });
+        }
+
+        $(document).ready(function () {   
+            initMap(); // Llama a la función initMap para inicializar el mapa
+            setInterval(reloadTable, 7000);
+        });
