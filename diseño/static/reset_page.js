de015111ff3ca898846fc7c631f826264366c0ed
Samuel Solano <114268063+Sdsolano@users.noreply.github.com>
2023-09-11 18:00:43 -0500

    Update reset_page.js

--- diseño/static/reset_page.js
+++ diseño/static/reset_page.js
@@ -2,19 +2,21 @@ var map;
 var marker;
 
 function initMap() {
+    // Inicializa el mapa
     map = L.map('map').setView([0, 0], 10); // Centro del mapa inicial y nivel de zoom
 
     // Agrega un mapa base de OpenStreetMap (puedes cambiarlo a otro proveedor de mapas)
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        maxZoom: 20, // Nivel de zoom máximo
-        minZoom: 12, // Nivel de zoom mínimo
+        maxZoom: 15, // Nivel de zoom máximo
+        minZoom: 5, // Nivel de zoom mínimo
     }).addTo(map);
 
-    marker = L.marker([0, 0]).addTo(map); // Utiliza el marcador por defecto
+    // Crea el marcador en el mapa
+    marker = L.marker([0, 0]).addTo(map);
 
     actualizarDatos(); // Llama a la función para cargar los datos y el mapa inicialmente
 
-    // Actualizar la tabla cada 5 segundos (5000 milisegundos)
+    // Actualiza la tabla y el marcador cada 5 segundos (5000 milisegundos)
     setInterval(actualizarDatos, 5000);
 }
 
@@ -28,9 +30,6 @@ function actualizarDatos() {
 
                 // Actualiza la posición del marcador con las coordenadas de la primera coordenada
                 marker.setLatLng([parseFloat(primeraCoordenada.Latitude), parseFloat(primeraCoordenada.Longitude)]);
-
-                // Centra el mapa en la nueva posición del marcador
-                map.setView([parseFloat(primeraCoordenada.Latitude), parseFloat(primeraCoordenada.Longitude)]);
             } else {
                 console.error("No se encontraron datos para mostrar en el mapa.");
             }
