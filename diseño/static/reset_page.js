07a88f2311901d040a479b87b37b66085dbd7a0b
Samuel Solano <114268063+Sdsolano@users.noreply.github.com>
2023-09-11 17:39:58 -0500

    Update reset_page.js

--- diseño/static/reset_page.js
+++ diseño/static/reset_page.js
@@ -22,5 +22,55 @@ function initMap() {
     marker = L.marker(fixedLatLng);
 }
 
-// Resto del código (reloadTable y document.ready) permanece igual
+// Función para cargar y actualizar la tabla
+function reloadTable() {
+    $.ajax({
+        url: "/components",
+        method: "GET",
+        success: function (response) {
+            // Verifica si la respuesta contiene datos
+            if (response.length > 0) {
+                // Actualiza la tabla con los últimos tres datos
+                var tablaHTML = "<table>";
+                tablaHTML += "<thead><tr><th>ID</th><th>Latitude</th><th>Longitude</th><th>Time_stamp</th></tr></thead>";
+                tablaHTML += "<tbody>";
+                for (var i = 0; i < Math.min(response.length, 3); i++) {
+                    var row = response[i];
+                    tablaHTML += "<tr><td>" + row.ID + "</td><td>" + row.Latitude + "</td><td>" + row.Longitude +  "</td><td>" + row.Time_stamp + "</td></tr>";
+                }
+                tablaHTML += "</tbody></table>";
 
+                $("#tabla-contenido").html(tablaHTML);
+
+                // Obtén las coordenadas de la primera fila de la tabla
+                var firstRow = response[0];
+                var latLng = [parseFloat(firstRow.Latitude), parseFloat(firstRow.Longitude)];
+
+                // Si el marcador ya está en el mapa, elimínalo antes de realizar el zoom
+                if (map.hasLayer(marker)) {
+                    map.removeLayer(marker);
+                }
+
+                // Actualiza la posición del marcador con las coordenadas de la primera fila
+                marker.setLatLng(latLng);
+
+                // Agrégalo nuevamente al mapa
+                marker.addTo(map);
+
+                // Centra el mapa en la ubicación de la primera fila
+                map.setView(latLng);
+            } else {
+                console.error("No se encontraron datos para mostrar en la tabla.");
+            }
+        },
+        error: function (xhr, status, error) {
+            console.error("AJAX request failed", error);
+        }
+    });
+}
+
+$(document).ready(function () {
+    initMap(); // Llama a la función initMap para inicializar el mapa (solo se ejecutará la primera vez)
+    reloadTable();
+    setInterval(reloadTable, 7000);
+});
