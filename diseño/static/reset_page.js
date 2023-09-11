bc02bd9df14ece3de4377d42dafa5d190c8684e5
Samuel Solano <114268063+Sdsolano@users.noreply.github.com>
2023-09-11 17:40:51 -0500

    Update reset_page.js

--- diseño/static/reset_page.js
+++ diseño/static/reset_page.js
@@ -1,76 +1,33 @@
 // Define variables globales para el mapa y el marcador
-let map;
-let marker;
+let map = null;
+let marker = null;
 
 // Coordenadas geográficas fijas para el marcador
 const fixedLatLng = [0, 0]; // Reemplaza estas coordenadas con las que desees
 
+// Bandera para verificar si el mapa ya se ha inicializado
+let mapInitialized = false;
+
 // Función para inicializar el mapa con Leaflet
 function initMap() {
-    // Inicializa el mapa con Leaflet
-    map = L.map('map').setView([0, 0], 13);
+    if (!mapInitialized) {
+        // Inicializa el mapa con Leaflet
+        map = L.map('map').setView([0, 0], 13);
 
-    // Deshabilita el zoom con la rueda del ratón
-    map.scrollWheelZoom.disable();
+        // Agrega un mapa base de OpenStreetMap (puedes cambiarlo a otro proveedor de mapas)
+        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            maxZoom: 19,
+        }).addTo(map);
 
-    // Agrega un mapa base de OpenStreetMap (puedes cambiarlo a otro proveedor de mapas)
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        maxZoom: 19,
-    }).addTo(map);
+        // Deshabilita el zoom con la rueda del ratón
+        map.scrollWheelZoom.disable();
 
-    // Crea un marcador en la posición geográfica fija, pero NO lo agregues al mapa aún
-    marker = L.marker(fixedLatLng);
+        // Crea un marcador en la posición geográfica fija, pero NO lo agregues al mapa aún
+        marker = L.marker(fixedLatLng);
+
+        mapInitialized = true; // Establece la bandera en verdadero para evitar futuras inicializaciones
+    }
 }
 
-// Función para cargar y actualizar la tabla
-function reloadTable() {
-    $.ajax({
-        url: "/components",
-        method: "GET",
-        success: function (response) {
-            // Verifica si la respuesta contiene datos
-            if (response.length > 0) {
-                // Actualiza la tabla con los últimos tres datos
-                var tablaHTML = "<table>";
-                tablaHTML += "<thead><tr><th>ID</th><th>Latitude</th><th>Longitude</th><th>Time_stamp</th></tr></thead>";
-                tablaHTML += "<tbody>";
-                for (var i = 0; i < Math.min(response.length, 3); i++) {
-                    var row = response[i];
-                    tablaHTML += "<tr><td>" + row.ID + "</td><td>" + row.Latitude + "</td><td>" + row.Longitude +  "</td><td>" + row.Time_stamp + "</td></tr>";
-                }
-                tablaHTML += "</tbody></table>";
+// Resto del código (reloadTable y document.ready) permanece igual
 
-                $("#tabla-contenido").html(tablaHTML);
-
-                // Obtén las coordenadas de la primera fila de la tabla
-                var firstRow = response[0];
-                var latLng = [parseFloat(firstRow.Latitude), parseFloat(firstRow.Longitude)];
-
-                // Si el marcador ya está en el mapa, elimínalo antes de realizar el zoom
-                if (map.hasLayer(marker)) {
-                    map.removeLayer(marker);
-                }
-
-                // Actualiza la posición del marcador con las coordenadas de la primera fila
-                marker.setLatLng(latLng);
-
-                // Agrégalo nuevamente al mapa
-                marker.addTo(map);
-
-                // Centra el mapa en la ubicación de la primera fila
-                map.setView(latLng);
-            } else {
-                console.error("No se encontraron datos para mostrar en la tabla.");
-            }
-        },
-        error: function (xhr, status, error) {
-            console.error("AJAX request failed", error);
-        }
-    });
-}
-
-$(document).ready(function () {
-    initMap(); // Llama a la función initMap para inicializar el mapa (solo se ejecutará la primera vez)
-    reloadTable();
-    setInterval(reloadTable, 7000);
-});
