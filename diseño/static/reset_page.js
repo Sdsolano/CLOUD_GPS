20101b5c415b866bad554d9c13d5865063eee86f
Samuel Solano <114268063+Sdsolano@users.noreply.github.com>
2023-09-11 17:55:46 -0500

    Update reset_page.js

--- diseño/static/reset_page.js
+++ diseño/static/reset_page.js
@@ -2,49 +2,35 @@ var map;
 var marker;
 
 function initMap() {
-    // Inicializa el mapa
-    map = L.map('map').setView([-0.5, 0.5], 13); // Coordenadas iniciales de ejemplo y nivel de zoom
+    map = L.map('map').setView([0, 0], 10); // Centro del mapa inicial y nivel de zoom
 
     // Agrega un mapa base de OpenStreetMap (puedes cambiarlo a otro proveedor de mapas)
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         maxZoom: 15, // Nivel de zoom máximo
-        minZoom: 12, // Nivel de zoom mínimo
+        minZoom: 5, // Nivel de zoom mínimo
     }).addTo(map);
 
-    // Crea el marcador en el mapa con una ubicación inicial
-    marker = L.marker([-0.5, 0.5]).addTo(map);
-    marker.bindPopup("Mi Marcador"); // Agrega un mensaje emergente al marcador
+    marker = L.marker([0, 0]).addTo(map); // Utiliza el marcador por defecto
 
-    // Carga la tabla y actualiza el mapa
-    reloadTable();
+    actualizarDatos(); // Llama a la función para cargar los datos y el mapa inicialmente
+
+    // Actualizar la tabla cada 5 segundos (5000 milisegundos)
+    setInterval(actualizarDatos, 5000);
 }
 
-function reloadTable() {
+function actualizarDatos() {
     $.ajax({
         url: "/components", // Reemplaza con la URL correcta para obtener datos
         method: "GET",
-        success: function (response) {
-            // Actualiza la tabla con los últimos tres datos
-            var tablaHTML = "<table>";
-            tablaHTML += "<thead><tr><th>ID</th><th>Latitude</th><th>Longitude</th><th>Time_stamp</th></tr></thead>";
-            tablaHTML += "<tbody>";
-            for (var i = 0; i < Math.min(response.length, 3); i++) {
-                var row = response[i];
-                tablaHTML += "<tr><td>" + row.ID + "</td><td>" + row.Latitude + "</td><td>" + row.Longitude +  "</td><td>" + row.Time_stamp + "</td></tr>";
-            }
-            tablaHTML += "</tbody></table>";
+        success: function (respuesta) {
+            if (respuesta.length > 0) {
+                var primeraCoordenada = respuesta[0];
 
-            $("#tabla-contenido").html(tablaHTML);
-
-            if (response.length > 0) {
-                var primeraCoordenada = response[0];
-
-                // Actualiza la posición del marcador con las coordenadas de la primera fila
+                // Actualiza la posición del marcador con las coordenadas de la primera coordenada
                 marker.setLatLng([parseFloat(primeraCoordenada.Latitude), parseFloat(primeraCoordenada.Longitude)]);
 
-                // Centra el mapa en la ubicación de la primera fila
+                // Centra el mapa en la nueva posición del marcador
                 map.setView([parseFloat(primeraCoordenada.Latitude), parseFloat(primeraCoordenada.Longitude)]);
-
             } else {
                 console.error("No se encontraron datos para mostrar en el mapa.");
             }
@@ -55,7 +41,7 @@ function reloadTable() {
     });
 }
 
-$(document).ready(function () {   
-    initMap(); // Llama a la función initMap para inicializar el mapa
-    setInterval(reloadTable, 7000);
+// Llama a la función initMap al cargar la página
+$(document).ready(function () {
+    initMap();
 });
