e0a1206ff6fe10a3736984ed9e431afcb0b45b07
Samuel Solano <114268063+Sdsolano@users.noreply.github.com>
2023-09-11 17:32:48 -0500

    Update reset_page.js

--- diseño/static/reset_page.js
+++ diseño/static/reset_page.js
@@ -1,22 +1,26 @@
 // Define variables globales para el mapa y el marcador
-let map;
-let marker;
+let map = null;
+let marker = null;
 
 // Coordenadas geográficas fijas para el marcador
 const fixedLatLng = [0, 0]; // Reemplaza estas coordenadas con las que desees
 
 // Función para inicializar el mapa con Leaflet
 function initMap() {
-    // Inicializa el mapa con Leaflet
-    map = L.map('map').setView([0, 0], 13);
+    if (map === null) {
+        // Inicializa el mapa con Leaflet solo si aún no está inicializado
+        map = L.map('map').setView([0, 0], 13);
 
-    // Agrega un mapa base de OpenStreetMap (puedes cambiarlo a otro proveedor de mapas)
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        maxZoom: 19,
-    }).addTo(map);
+        // Agrega un mapa base de OpenStreetMap (puedes cambiarlo a otro proveedor de mapas)
+        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            maxZoom: 19,
+        }).addTo(map);
+    }
 
-    // Crea un marcador en la posición geográfica fija, pero NO lo agregues al mapa aún
-    marker = L.marker(fixedLatLng);
+    if (marker === null) {
+        // Crea un marcador en la posición geográfica fija, pero NO lo agregues al mapa aún
+        marker = L.marker(fixedLatLng);
+    }
 }
 
 // Función para cargar y actualizar la tabla
@@ -64,7 +68,7 @@ function reloadTable() {
 }
 
 $(document).ready(function () {
-    initMap(); // Llama a la función initMap para inicializar el mapa
+    initMap(); // Llama a la función initMap para inicializar el mapa (solo se ejecutará la primera vez)
     reloadTable();
     setInterval(reloadTable, 7000);
 });
