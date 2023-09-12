c72396cf90af0bc2a8f33e9a46f055b9ecf6b00d
Samuel Solano <114268063+Sdsolano@users.noreply.github.com>
2023-09-11 20:03:53 -0500

    Update reset_page.js

--- diseño/static/reset_page.js
+++ diseño/static/reset_page.js
@@ -64,6 +64,7 @@ function reloadTable() {
 }
 
 
+  $(document).ready(function () {   
     initMap(); // Llama a la función initMap para inicializar el mapa
     setInterval(reloadTable, 1000);
-
+});
