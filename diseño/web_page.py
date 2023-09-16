e6edaba6382cdc22c32137ae20a10a20bd3a6c20
Kenneth3750 <110206946+Kenneth3750@users.noreply.github.com>
2023-09-15 19:36:01 -0500

    Update web_page.py

--- diseño/web_page.py
+++ diseño/web_page.py
@@ -50,9 +50,33 @@ def data():
     except Exception as e:
         return "Error " + str(e)
 
+def obtener_valores_historicos():
+    if request.method == 'POST':
+        fecha_inicio = request.form.get('fecha_inicio')
+        fecha_fin = request.form.get('fecha_fin')
+
+        # Ahora puedes utilizar fecha_inicio y fecha_fin en tu lógica
+        # para procesar los valores del formulario como lo necesites.
+
+        # Por ejemplo, puedes imprimirlos en la consola
+        print("Fecha de inicio:", fecha_inicio)
+        print("Fecha de fin:", fecha_fin)
+
+        # También puedes realizar cualquier otro procesamiento que necesites aquí
+
+    # Redirecciona a la página históricos o muestra una respuesta adecuada
+    return render_template("historicos.html")
+
+
+
+
 @app.route('/')
 def index():
     return render_template("index.html", google_maps_api_key=GOOGLE_MAPS_API_KEY)
+
+
+
+
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=80)
