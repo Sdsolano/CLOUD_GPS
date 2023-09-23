9adc4549606d1d3cc078b794f8a644b78661562a
Kenneth3750 <110206946+Kenneth3750@users.noreply.github.com>
2023-09-23 13:16:45 -0500

    submit
    agregue un submit

--- diseño/static/reset_page.js
+++ diseño/static/reset_page.js
@@ -94,6 +94,8 @@ function initMap3() {
     };
     $("#titulo-fechas").html("When did my vehicle pass through here?")
     $("#parrafo-fechas").html("Right-click on the map over the place you want to know when your vehicle passed through. The dates are limited between the dates of the previous search.");
+    $("#parrafo2-fechas").html("Select a radius between xx and xx");
+    $("#slider-text").show(); 
     map3 = new google.maps.Map(document.getElementById('mapa-fechas'), mapOptions);
 
     // Realiza una solicitud AJAX para obtener la última posición desde la base de datos
