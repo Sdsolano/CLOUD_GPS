77dcc307f3991edd510821a3c3049894df53345c
Samuel Solano <sdsolano@uninorte.edu.co>
2023-09-24 17:05:57 -0500

    Update reset_page.js

--- diseño/static/reset_page.js
+++ diseño/static/reset_page.js
@@ -482,7 +482,7 @@ $(document).ready(function () {
     });
 
     $("#buscar").on("click", function () {
-        
+        markersWithinCircle = []; 
         if (!circle) {
             alert("Please set the circle on the map first.");
             return;
