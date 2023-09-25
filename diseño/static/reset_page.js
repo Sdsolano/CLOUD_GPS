3fc8c846777094cdd6068ebeadc47e5ee1742615
Samuel Solano <sdsolano@uninorte.edu.co>
2023-09-25 01:14:29 -0500

    Update reset_page.js

--- diseño/static/reset_page.js
+++ diseño/static/reset_page.js
@@ -561,6 +561,20 @@ $(document).ready(function () {
     
     
     function updateMarker(index) {
+        var iconSettings = {
+            path: 'M25.395,0H17.636c-3.117,0-5.643,3.467-5.643,6.584v34.804c0,3.116,2.526,5.644,5.643,5.644h11.759' +
+                'c3.116,0,5.644-2.527,5.644-5.644V6.584C35.037,3.467,32.511,0,29.395,0z M34.05,14.188v11.665l-2.729,0.351v-4.806L34.05,14.188z' +
+                'M32.618,10.773c-1.016,3.9-2.219,8.51-2.219,8.51H16.631l-2.222-8.51C14.41,10.773,23.293,7.755,32.618,10.773z M15.741,21.713' +
+                'v4.492l-2.73-0.349V14.502L15.741,21.713z M13.011,37.938V27.579l2.73,0.343v8.196L13.011,37.938z M14.568,40.882l2.218-3.336' +
+                'h13.771l2.219,3.336H14.568z M31.321,35.805v-7.872l2.729-0.355v10.048L31.321,35.805z',
+            fillColor: '#FFC02B',
+            fillOpacity: 1,
+            scale: 0.6,
+            strokeColor: 'black',
+            strokeWeight: 1.5,
+            anchor: new google.maps.Point(23, 0),
+            rotation: 90
+        };
         // Remove the existing marker if present
         if (currentMarker) {
             currentMarker.setMap(null);
@@ -575,7 +589,7 @@ $(document).ready(function () {
                 position: coordinateLatLng,
                 map: map3,
                 title: "Coordinate " + index,
-                icon: 'http://maps.google.com/mapfiles/ms/icons/blue-dot.png'
+                icon: iconSettings,
                 // You can add more information to the marker, e.g., timestamp
                 //label: timestamp,
             });
