3d7595973f93fbe6bf6f20bcad765af90608dd0e
Samuel Solano <sdsolano@uninorte.edu.co>
2023-09-25 01:02:03 -0500

    Update reset_page.js

--- diseño/static/reset_page.js
+++ diseño/static/reset_page.js
@@ -317,7 +317,7 @@ function actualizarHistoricosData(data, indexCenter) {
         fillOpacity: 1,
         scale: 0.6,
         strokeColor: 'black',
-        strokeWeight: 4,
+        strokeWeight: 2,
         anchor: new google.maps.Point(23, 0),
         rotation: 90
     };
