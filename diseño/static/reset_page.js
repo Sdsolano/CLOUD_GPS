e4adfb0fd4561e51ef1a2dc7984a21d06239f1de
Samuel Solano <sdsolano@uninorte.edu.co>
2023-09-25 20:58:36 -0500

    Update reset_page.js

--- diseño/static/reset_page.js
+++ diseño/static/reset_page.js
@@ -201,7 +201,7 @@ function circlechanger (radius){
         position = markerDates.getPosition();
         if (circle) {
             circle.setMap(null); // Remove the existing circle if any
-            circle = new google.maps.Circle({
+            circle2 = new google.maps.Circle({
                 strokeColor: '#1C2F4F',
                 strokeOpacity: 0.8,
                 strokeWeight: 2,
