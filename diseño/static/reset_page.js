7dd0ba05d755c49cc52d79486a6da845cf2b60be
Samuel Solano <sdsolano@uninorte.edu.co>
2023-09-25 21:01:39 -0500

    Update reset_page.js

--- diseño/static/reset_page.js
+++ diseño/static/reset_page.js
@@ -198,20 +198,22 @@ function initMap3() {
 }
 
 function circlechanger (radius){
-        position = markerDates.getPosition();
+
         if (circle) {
             circle.setMap(null); // Remove the existing circle if any
-            circle2 = new google.maps.Circle({
-                strokeColor: '#1C2F4F',
-                strokeOpacity: 0.8,
-                strokeWeight: 2,
-                fillColor: 'red',
-                fillOpacity: 0.35,
-                map: map3,
-                center: position,
-                radius: radius,
-            });
         }
+        position = markerDates.getPosition();
+        
+        circle2 = new google.maps.Circle({
+            strokeColor: '#1C2F4F',
+            strokeOpacity: 0.8,
+            strokeWeight: 2,
+            fillColor: 'red',
+            fillOpacity: 0.35,
+            map: map3,
+            center: position,
+            radius: radius,
+        });
 }
 
 
